4b441a9f513eb136ded7202dfb4f77ea27dffcb5
Cameron <cameronkwalden@gmail.com>
2021-10-25 21:01:45 -0700

    initial commit

---
+++ src/routes/sign-in.js
@@ -0,0 +1,37 @@
+'use strict'; 
+
+const express = require('express');
+const app = express();
+const base64 = require('base-64');
+const bcrypt = require('bcrypt');
+const authorizationRoute = require('../middleware/password.js');
+
+const { User } = require('../models/index.js')
+const router = express.Router();
+
+router.post('/signin', signIn);
+// router.post('/signin', authorizationRoute, signIn);
+
+async function signIn(request, response) {
+  try {
+    let authString = request.headers.authorization;
+    let encodedUserPassword = authString.split(' ')[1];
+    let decodedUserPassword = base64.decode(encodedUserPassword);
+    let [ user, password ] = decodedUserPassword.split(':')
+
+    let userQuery = await User.findOne({ where: {
+      username: user
+    }
+  });
+  let isValidPassword = await bcrypt.compare(password, userQuery.password);
+    if(isValidPassword) {
+      response.send(userQuery);
+    } else {
+      response.status(401).send('Unauthenticated');
+    }
+  } catch(error) {
+    response.status(401).send('Unauthenticated request');
+  }
+}
+
+module.exports = router;
